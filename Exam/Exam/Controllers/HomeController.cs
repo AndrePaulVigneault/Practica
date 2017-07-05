5830b1477f1f555c2672cf26cad408ed57582eba
AndrePaulVigneault <apvg1990@hotmail.com>
2017-07-05 14:06:33 -0600

    Added initial version
    Added initial version

---
+++ Exam/Exam/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Exam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exam.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ExamContext db_context = new ExamContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAuthors() {
+            return Json(db_context.Authors.ToList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
